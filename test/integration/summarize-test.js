6924458ce8e6a33d491c45d86576130cf5bbef6e
simonihmig <simon.ihmig@kaliber5.de>
2018-11-05 00:22:14 +0100

    Handle uglify errors
    Fixes #39

--- test/integration/summarize-test.js
+++ test/integration/summarize-test.js
@@ -4,9 +4,11 @@ const chai = require('chai');
 const expect = chai.expect;
 const tmp = require('tmp');
 const chaiFiles = require('chai-files');
+const chaiAsPromised = require('chai-as-promised');
 const file = chaiFiles.file;
 const path = require('path');
 const copyFixtures = require('../helpers/copy-fixtures');
+const fs = require('fs-extra');
 
 const inputFixturePath = 'test/fixtures/input';
 const outputFixturePath = 'test/fixtures/output';
@@ -15,6 +17,7 @@ const summarize = require('../../lib/summarize');
 const summarizeAll = require('../../lib/summarize-all');
 
 chai.use(chaiFiles);
+chai.use(chaiAsPromised);
 
 describe('summarize', function() {
   let tmpPath;
@@ -32,6 +35,19 @@ describe('summarize', function() {
       copyFixtures(inputFile, inputFixturePath, tmpPath);
       return summarize(path.join(tmpPath, `${inputFile}.json`))
         .then(() => expect(file(path.join(tmpPath, outputFile))).to.equal(file(path.join(outputFixturePath, outputFile))));
+    });
+
+    it('rejects promise on minification errors', function() {
+      let inputFile = '1-test-app.js';
+
+      copyFixtures(inputFile, inputFixturePath, tmpPath);
+      // this will cause terser to fail
+      fs.writeFileSync(path.join(tmpPath, inputFile, 'test-app/app.js'), 'foo(;', { flag: 'a' });
+
+      let promise = summarize(path.join(tmpPath, `${inputFile}.json`));
+      return expect(promise).to.be.rejected
+        .and.to.eventually.have.property('message')
+        .match(/Unexpected token/);
     });
 
   });
