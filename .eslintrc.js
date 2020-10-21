f35af890651bf96fe5227763ed5a8464a22eb8f0
Aleksandr Kanunnikov <lifeart92@gmail.com>
2020-10-21 17:24:22 +0300

    node bump from 6 to 10

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,16 @@ module.exports = {
   "extends": "eslint:recommended",
   "plugins": ["jest"],
   "parserOptions": {
-    "ecmaVersion": 6
+    "ecmaVersion": 2018
   },
   "env": {
     "node": true,
     "es6": true,
     "jest": true
   },
+  "ignorePatterns": [
+    "test/fixtures/input"
+  ],
   "rules": {
   }
 };
